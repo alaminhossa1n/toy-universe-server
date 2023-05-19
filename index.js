5572d358d033e82e7f21d0315828a7f76fc91887
alaminhossa1n <alaminatghoria@gmail.com>
2023-05-19 15:44:27 +0600

    delete method okay

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json())
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xsalsjk.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -26,7 +26,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
@@ -68,16 +68,16 @@ async function run() {
         }
     })
 
+    // delete toy by id
+    app.delete('/toys/:id', async (req, res) => {
+        id = req.params.id
+        const query = { _id: new ObjectId(id) }
+        const result = await toysCollections.deleteOne(query)
+        res.send(result)
+    })
+
 }
 run().catch(console.dir);
-
-
-
-
-
-
-
-
 
 
 app.get('/', (req, res) => {
